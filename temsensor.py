946307bd3c9ec13e9a6b74634c8661141cb6963d
ZYU <zhy125@ucsd.edu>
2018-08-19 23:35:39 +0000

    init

---
+++ temsensor.py
@@ -0,0 +1,58 @@
+import os
+import glob
+import time
+ 
+os.system('modprobe w1-gpio')
+os.system('modprobe w1-therm')
+ 
+base_dir = '/sys/bus/w1/devices/'
+device_folder = glob.glob(base_dir + '28-011832501aff')[0]
+device_file = device_folder + '/w1_slave'
+
+device_folder1 = glob.glob(base_dir + '28-01183254a6ff')[0]
+device_file1 = device_folder1 + '/w1_slave'
+
+def read_temp_raw():
+    f = open(device_file, 'r')
+    lines = f.readlines()
+    f.close()
+    return lines
+
+def read_temp_raw1():
+    f = open(device_file1, 'r')
+    lines = f.readlines()
+    f.close()
+    return lines
+
+
+def read_temp():
+    lines = read_temp_raw()
+    while lines[0].strip()[-3:] != 'YES':
+        time.sleep(0.2)
+        lines = read_temp_raw()
+    equals_pos = lines[1].find('t=')
+    if equals_pos != -1:
+        temp_string = lines[1][equals_pos+2:]
+        temp_c = float(temp_string) / 1000.0
+        temp_f = temp_c * 9.0 / 5.0 + 32.0
+        return temp_c, temp_f
+
+def read_temp1():
+    lines = read_temp_raw1()
+    while lines[0].strip()[-3:] != 'YES':
+        time.sleep(0.2)
+        lines = read_temp_raw()
+    equals_pos = lines[1].find('t=')
+    if equals_pos != -1:
+        temp_string = lines[1][equals_pos+2:]
+        temp_c = float(temp_string) / 1000.0
+        temp_f = temp_c * 9.0 / 5.0 + 32.0
+        return temp_c, temp_f
+
+
+while True:
+	print 'temp1 =',
+        print read_temp(),
+        print 'temp2 =',
+        print read_temp1()
+	time.sleep(1)
